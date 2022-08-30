fc51b22420186b2d6bf4c220cb4a52f99b381eeb
Je-Carlos <cpjeanberg@gmail.com>
2022-08-30 14:08:42 -0300

    Implementando filtros no cardápio

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,6 +2,7 @@ import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
 import Buscador from "./Buscador";
 import { useState } from "react";
+import Filtros from "./Filtros";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
@@ -18,6 +19,10 @@ export default function Cardapio() {
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca}/>
+        {/* Inserindo filtros no cardapio */}
+        <div className={styles.cardapio__filtros}> 
+        <Filtros/>
+        </div>
       </section>
     </main>
   )
