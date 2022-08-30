cec9009e177225b3af195f957c5d2b949192bec8
Je-Carlos <cpjeanberg@gmail.com>
2022-08-30 14:47:37 -0300

    Refinando o filtro do cardápio

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -6,24 +6,23 @@ import Filtros from "./Filtros";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
   return (
     <main>
       <nav className={styles.menu}>
         <Logo />
       </nav>
       <header className={styles.header}>
-        <div className={styles.header__text}>
-          A casa do código e da massa
-        </div>
+        <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-        <Buscador busca={busca} setBusca={setBusca}/>
+        <Buscador busca={busca} setBusca={setBusca} />
         {/* Inserindo filtros no cardapio */}
-        <div className={styles.cardapio__filtros}> 
-        <Filtros/>
+        <div className={styles.cardapio__filtros}>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
         </div>
       </section>
     </main>
-  )
+  );
 }
